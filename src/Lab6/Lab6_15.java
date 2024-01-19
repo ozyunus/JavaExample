a0021442d7e3efb01c0f635281f7ade26fdb714d
Yunus Oz <ozyunus@msn.com>
2024-01-19 20:56:30 +0300

    -

--- src/Lab6/Lab6_15.java
+++ src/Lab6/Lab6_15.java
@@ -4,22 +4,19 @@ public class Lab6_15 {
 
 	public static void main(String[] args) {
 		int[] myArray = {2,3,5,7,8,9,11,12,14};
-		int asal =0;
-		boolean isPrime = true;
 		
-		for (int i=2; i<myArray.length; i++) {
-			for (int j=2; j<=i; j++) {
+		for (int i=0; i<myArray.length; i++) {
+			boolean isPrime = true;
+			for (int j=2; j<myArray[i]; j++) {
+				
 				if(myArray[i]%j==0)
 				{
 					isPrime=false;
 					break;
 				}
-				else {
-					asal = myArray[i];
-				}
 			}
 			if (isPrime) {
-				System.out.println(asal+" asal sayıdır.");
+				System.out.println(myArray[i]+" asal sayıdır.");
 			}
 			}
 	}
