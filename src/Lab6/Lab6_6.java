cfd2b3a129a516ac861efd7f16bd77d468c5a71e
Yunus Oz <ozyunus@msn.com>
2024-01-25 16:09:23 +0300

    -

--- src/Lab6/Lab6_6.java
+++ src/Lab6/Lab6_6.java
@@ -3,9 +3,9 @@ package Lab6;
 public class Lab6_6 {
 
 	public static void main(String[] args) {
-		int[] myArray = {-5,-6,-4343,5,6,8,999};
-		int maxValue =0;
-		int minValue =0;
+		int[] myArray = {-5,-6,-4343,5,-6,-8,-999};
+		int maxValue =myArray[0];
+		int minValue =myArray[0];
 		
 		for (int i=0; i<myArray.length; i++) {
 			if (myArray[i]<minValue) {
