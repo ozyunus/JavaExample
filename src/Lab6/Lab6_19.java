b3fc70bdba5926460ab8bcf36b51cd581d26d902
Yunus Oz <ozyunus@msn.com>
2024-01-26 12:11:43 +0300

    -

--- src/Lab6/Lab6_19.java
+++ src/Lab6/Lab6_19.java
@@ -1,29 +1,46 @@
 package Lab6;
 
 import java.util.Arrays;
+import java.util.Iterator;
+
 
 public class Lab6_19 {
 
 	public static void main(String[] args) {
-		int[] myArray = {4,5,6,7,3,4};
-		int count = 0;
-		int[] numbersArray = new int[myArray.length];
+		int[] myArray = {4,5,6,7,3,4,8};
+		int[] farkliSayilar = new int[myArray.length];
 		int[] countsArray = new int[myArray.length];
+		int sayac = 0;
 		
 		for (int i=0; i<myArray.length; i++) {
-			
+			int count=0;
+
+			boolean varmi = false;
+			for(int j=0; j<farkliSayilar.length; j++) {
+				if (farkliSayilar[j] ==myArray[i])
+					varmi = true;
+			}
+		
+			if(varmi == false) {
+				farkliSayilar[sayac]=myArray[i];
+			}
+
 			for(int j=0; j<myArray.length; j++) {
 				if (myArray[i]==myArray[j]) {
-					numbersArray[i]=myArray[i];
-					countsArray[i]+=1; 
+					count++;
 				}
 			}
+			if(varmi == false) {
+				countsArray[sayac]=count;
+				sayac++;
+			}
 		}
-		System.out.print("count "+Arrays.toString(countsArray)+", ");
-		System.out.print("\nnumara "+Arrays.toString(numbersArray)+", ");
+		System.out.println("Dizi: "+Arrays.toString(myArray));
+		for (int i= 0; i<sayac; i++) {
+			System.out.println(farkliSayilar[i]+"= "+countsArray[i]+" kez tekrar etti.");
+		}
 	}
 
 }
 
 
-// tek tek yazdırma ile ilgili algoritmayı düşünemedim :)
