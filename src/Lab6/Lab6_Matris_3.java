4fcbad6816603b2e95de0bb6ff31df6ace9a9e4e
Yunus Oz <ozyunus@msn.com>
2024-03-08 16:23:20 +0300

    -

---
+++ src/Lab6/Lab6_Matris_3.java
@@ -0,0 +1,27 @@
+package Lab6;
+
+import java.util.Iterator;
+
+public class Lab6_Matris_3 {
+
+	public static void main(String[] args) {
+		int total =0;
+
+		int[][] table = new int [][] {
+			{1,2,-3},
+			{-1,3,5},
+			{-9,2,3}
+		};
+				
+		for (int i=0; i<table.length; i++) {
+			for (int j=0; j<table[i].length; j++) {
+				if (table[i][j]<0) {
+					total += table[i][j];
+				}
+			}
+		}
+		System.out.println(total);
+
+	}
+
+}
