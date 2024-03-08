784ca3066defc5b03d2fb271e4b19418d5601e38
Yunus Oz <ozyunus@msn.com>
2024-03-08 16:48:32 +0300

    -

---
+++ src/Lab6/Lab6_Matris_4.java
@@ -0,0 +1,27 @@
+package Lab6;
+
+public class Lab6_Matris_4 {
+
+	public static void main(String[] args) {
+		int count =0;
+		int[][] table = new int[][]{
+			{3,4,0},
+			{0,0,5},
+			{0,0,5},
+			{0,0,6},
+			{0,0,3},
+			{0,6,0}
+		};
+		for (int i=0; i<table.length; i++) {
+			for (int j=0; j<table[i].length; j++) {
+				if(table[i][j]==0) {
+					count++;
+				}
+			}
+		}
+		if (table.length/2 <count) {
+			System.out.println("Sparse");
+		}
+	}
+
+}
