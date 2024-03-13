396810af117b6884e0880a04fcd3dd063d854400
Yunus Oz <ozyunus@msn.com>
2024-03-13 13:51:21 +0300

    -

---
+++ src/Lab6/Lab6_Matris_14.java
@@ -0,0 +1,21 @@
+package Lab6;
+
+public class Lab6_Matris_14 {
+
+	public static void main(String[] args) {
+		int total = 0;
+		int[][] table= {{1,2,3,4},
+						{5,6,7,8},
+						{9,10,11,12},
+						{13,14,15,16}
+		};
+		
+		for (int i = 0; i < table.length; i++) {
+			for (int j = 0; j <= i; j++) {
+	            total += table[i][j];
+	        }
+	    }
+		System.out.println("Toplam: "+total);
+	}
+
+}
