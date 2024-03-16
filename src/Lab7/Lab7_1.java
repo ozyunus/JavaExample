b39b4d228f7fe8d59b0591143a19f034e80bd5a7
Yunus Oz <ozyunus@msn.com>
2024-03-16 17:25:45 +0300

    -

---
+++ src/Lab7/Lab7_1.java
@@ -0,0 +1,28 @@
+package Lab7;
+
+import java.util.Scanner;
+
+public class Lab7_1 {
+
+	
+	public static void main(String[] args) {
+		Scanner inputScanner = new Scanner(System.in);
+		System.out.println("1.sayıyı giriniz: ");
+		int number1 = inputScanner.nextInt();
+		
+		System.out.println("2.sayıyı giriniz: ");
+		int number2 = inputScanner.nextInt();
+		
+		System.out.println("3.sayıyı giriniz: ");
+		int number3 = inputScanner.nextInt();
+		
+		avarage(number1, number2, number3);
+
+}
+	public static void avarage(int num1, int num2, int num3) {
+		int total = num1+num2+num3;
+		double avarage = total /3;
+		
+		System.out.println("Ortalama: "+avarage);
+	}
+}
