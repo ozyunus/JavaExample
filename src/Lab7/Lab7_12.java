8911cb7cb2d61d7aa2eae002feef29bc0aa0c1d0
Yunus Oz <ozyunus@msn.com>
2024-03-18 14:11:47 +0300

    -

---
+++ src/Lab7/Lab7_12.java
@@ -0,0 +1,25 @@
+package Lab7;
+
+public class Lab7_12 {
+
+	public static void main(String[] args) {
+		int[] table = {-82,3545564,4,5};
+		int tableLength = table.length;
+		
+		int maksimumSayi = maksimumuBul(table, tableLength);
+		System.out.println("Maksimum: "+maksimumSayi);
+	}
+	
+	public static int maksimumuBul(int[] arrays, int tLength) {
+		int max=arrays[0];
+		
+		for (int i=0; i<tLength; i++) {
+			if(arrays[i]>max) {
+				max = arrays[i];
+			}
+		}
+		
+		return max;
+	}
+
+}
