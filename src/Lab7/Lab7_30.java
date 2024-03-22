441eb03624719fe6ccae801f2b601507dfd5f3c3
Yunus Oz <ozyunus@msn.com>
2024-03-22 13:38:15 +0300

    -

--- src/Lab7/Lab7_30.java
+++ src/Lab7/Lab7_30.java
@@ -8,33 +8,33 @@ public class Lab7_30 {
 		Scanner inputScanner = new Scanner(System.in);
 		System.out.println("Bir sayı giriniz:");
 		int number = inputScanner.nextInt();
-		
+
 		int jenericNumber = jenerikDondur(number);
 		System.out.println(jenericNumber);
-	
+
 	}
+
 	public static int jenerikDondur(int number) {
-		int total=0;
-		int kalan =0;
-		int jenerik=0;
-		
-		while(number>0) {
-			kalan = number%10;
-			number-=kalan;
-			total+=kalan;
-			kalan=0;
-			number/=10;
+		int total = 0;
+		int kalan = 0;
+		int jenerik = 0;
+
+		while (number > 0) {
+			kalan = number % 10;
+			number -= kalan;
+			total += kalan;
+			kalan = 0;
+			number /= 10;
 		}
-		
-		while(total>0) {
-			kalan = total%10;
-			total-=kalan;
-		    jenerik+=kalan;
-			kalan=0;
-			total/=10;
+
+		while (total > 0) {
+			kalan = total % 10;
+			total -= kalan;
+			jenerik += kalan;
+			kalan = 0;
+			total /= 10;
 		}
-		
-		
+
 		return jenerik;
 	}
 }
