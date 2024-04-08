3937baf49784cf8d0abe54e25b8d1b5a9ac33794
Yunus Oz <ozyunus@msn.com>
2024-04-08 15:47:25 +0300

    -

---
+++ src/Strings/String_25.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class String_25 {
+
+	public static void main(String[] args) {
+		char[] karakter = {'s','e','l','a','m','k','e'};
+		String kelime="";
+		
+		kelime =String.valueOf(karakter);
+		System.out.println(kelime);
+		
+		String kelimem = new String(karakter);
+		System.out.println(kelimem);
+
+	}
+
+}
