22bf99de1054bbf5bffb12a4002272e9493c2d20
Yunus Oz <ozyunus@msn.com>
2024-04-08 15:04:14 +0300

    -

---
+++ src/Strings/String_22.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class String_22 {
+
+	public static void main(String[] args) {
+		String str = "Bugün hava çok güzel !"; 
+		String[] words = str.split(" ");
+		
+		for(String word: words) {
+			for (int i=0; i<word.length(); i++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
