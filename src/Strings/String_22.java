7285ccdef45fdfd23e4afcfaab84e27353c2fdd3
Yunus Oz <ozyunus@msn.com>
2024-04-08 15:04:27 +0300

    -

--- src/Strings/String_22.java
+++ src/Strings/String_22.java
@@ -3,7 +3,7 @@ package Strings;
 public class String_22 {
 
 	public static void main(String[] args) {
-		String str = "Bugün hava çok güzel !"; 
+		String str = "Bugün hava çok güzel!"; 
 		String[] words = str.split(" ");
 		
 		for(String word: words) {
