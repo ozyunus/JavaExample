0468ff534961cb924f6dd8d19eabce35c5d273d1
Yunus Oz <ozyunus@msn.com>
2024-04-08 14:58:03 +0300

    -

---
+++ src/Strings/String_21.java
@@ -0,0 +1,19 @@
+package Strings;
+
+import java.util.Iterator;
+import java.util.Spliterator;
+
+public class String_21 {
+
+	public static void main(String[] args) {
+		String inputTextString = " hello  selam merhaba";
+	
+		String[] newTextStrings = inputTextString.split(" ");
+		
+		for(String word:newTextStrings) {
+			System.out.println(word.trim());
+		}
+		
+	}
+
+}
