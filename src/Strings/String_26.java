5783c6115fd350d11724f86b7214ac02e58cc0fe
Yunus Oz <ozyunus@msn.com>
2024-04-22 16:39:39 +0300

    -

--- src/Strings/String_26.java
+++ src/Strings/String_26.java
@@ -3,13 +3,20 @@ package Strings;
 public class String_26 {
 
 	public static void main(String[] args) {
-		String[] words = {"ekmek","aş","su","yoldaş"};
+		String[] words = {"ekmek","aş","sllu","yoldaş"};
 		String word = "su";
+		int count=0;
 		
 		for (int i=0; i<words.length; i++) {
 			if(words[i]==word) {
-				System.out.println(word+" kelimesi dizide de vardır.");
+				count++;
 			}
+		}
+		if(count>0) {
+			System.out.println(word+" kelimesi dizide de vardır.");
+		}
+		else {
+			System.out.println(word+" kelimesi dizide yoktur.");
 		}
 
 	}
